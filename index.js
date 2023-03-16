ee341e66c86c6dfa3ceb476701b264a782bb2b6c
murad <mdmurad1685@gmail.com>
2023-03-16 12:31:07 -0300

    successfully end rest api without real database or using fake database

--- index.js
+++ index.js
@@ -4,29 +4,62 @@ const express = require('express');
 
 
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const products = data.products;
 
 
 const server = express();
+
+// body parser
+server.use(express.json());
 
 server.use((req, res, next) => {
     console.log(req.ip, req.method, req.hostname, req.get('User-Agent'));
     next();
 })
 
-server.get('/', (req, res) => {
-    res.json({ type: "GET" })
+server.get('/products', (req, res) => {
+    res.json(products);
 });
 
-server.post('/', (req, res) => {
-    res.json({ type: "Post" })
+server.get('/products/:id', (req, res) => {
+
+    const id = +req.params.id;
+    const prod = products.find(p => p.id === id);
+    res.json(prod);
 });
 
-server.put('/', (req, res) => {
-    res.json({ type: "Put" })
+//post product
+server.post('/createProduct', (req, res) => {
+    console.log(req.body);
+    products.push(req.body);
+    res.json(req.body);
 });
 
-server.delete('/', (req, res) => {
-    res.json({ type: "Delete" })
+//update product
+server.put('/updateProduct/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    products.splice(productIndex, 1, { ...req.body, id: id })
+    res.status(201).json();
+});
+
+//patch product
+server.patch('/patchProduct/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    const product = products[productIndex]
+    products.splice(productIndex, 1, { ...product, ...req.body })
+    console.log(req.body);
+    res.status(201).json();
+});
+
+
+server.delete('/deleteProduct/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    const product = products[productIndex];
+    products.splice(productIndex, 1)
+    res.status(201).json(product);
 });
 
 
